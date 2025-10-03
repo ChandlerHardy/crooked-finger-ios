b378cc95387d196bd3564b96687e3184884af8ac
Chandler Hardy <hardych04@gmail.com>
2025-10-03 16:25:13 -0500

    Initial Commit

---
+++ Crooked Finger iOS/Crooked_Finger_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crooked_Finger_iOSApp.swift
+//  Crooked Finger iOS
+//
+//  Created by Chandler Hardy on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Crooked_Finger_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Crooked Finger iOSTests/Crooked_Finger_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Crooked_Finger_iOSTests.swift
+//  Crooked Finger iOSTests
+//
+//  Created by Chandler Hardy on 10/3/25.
+//
+
+import Testing
+@testable import Crooked_Finger_iOS
+
+struct Crooked_Finger_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
